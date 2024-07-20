c9b2de0652b17e9cc9b718d8faa6751d8783638b
Rodinamomen <Rodinamobark3@gmail.com>
2024-07-20 09:04:13 -0700

    testing log

--- app/src/main/java/com/example/bookie/MainActivity.kt
+++ app/src/main/java/com/example/bookie/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bookie
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,5 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("testing log", "onCreate: ")
     }
 }
