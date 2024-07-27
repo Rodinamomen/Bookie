3967ece3b4e99d21405b8dadd5463a5864ae3d87
Mona Adel <96449268+MonaAdel2@users.noreply.github.com>
2024-07-27 17:41:01 +0300

    Implement sign up with google credentials.

--- app/src/main/java/com/example/bookie/auth/signup/repo/SignUpRepo.kt
+++ app/src/main/java/com/example/bookie/auth/signup/repo/SignUpRepo.kt
@@ -1,10 +1,21 @@
 package com.example.bookie.auth.signup.repo
 
+import android.content.Intent
 import androidx.lifecycle.LiveData
 import com.example.bookie.auth.signup.model.User
+import com.google.firebase.auth.FirebaseUser
 
 interface SignUpRepo {
 
-    suspend fun CreateNewUser(newUser: User, callback: (Boolean, String?) -> Unit)
+    val user: LiveData<FirebaseUser?>
+
+    suspend fun createNewUser(newUser: User, password: String, callback: (Boolean, String?) -> Unit)
+
+    suspend fun handleSignInResult(data: Intent?): FirebaseUser?
+    fun getGoogleSignInIntent(): Intent
+
+
+
+
 
 }
