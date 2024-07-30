2c70520a5ae55947ed6e1c64811dfce3bfdfc63e
Rodinamomen <rodinamobark3@gmail.com>
2024-07-30 01:18:36 -0700

    fetching data from api

--- app/src/main/java/com/example/bookie/network/ApiService.kt
+++ app/src/main/java/com/example/bookie/network/ApiService.kt
@@ -1,8 +1,10 @@
 package com.example.bookie.network
-import com.example.bookie.network.model.BooksResponse
+import com.example.bookie.BuildConfig
+import com.example.bookie.network.model.BookRespone
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
-    @GET("search.json?q=title:&limit=50")
-    suspend fun getBooksFromRemote(): BooksResponse
+    @GET("v1/volumes")
+    suspend fun getBooksFromRemote(@Query("key") apiKey: String , @Query("q") q:String): BookRespone
 }
