28e197e366841801074863763d35205ad5e1bf78
Rodinamomen <rodinamobark3@gmail.com>
2024-07-30 01:21:17 -0700

    fetching data from api

--- app/src/main/java/com/example/bookie/network/ApiClient.kt
+++ app/src/main/java/com/example/bookie/network/ApiClient.kt
@@ -3,12 +3,12 @@ package com.example.bookie.network
 import android.annotation.SuppressLint
 import android.util.Log
 import com.example.bookie.BuildConfig
-import com.example.bookie.network.model.BookRespone
+import com.example.bookie.network.model.BookResponse
 
 
 object ApiClient:RemoteDataSource {
     @SuppressLint("SuspiciousIndentation")
-    override suspend fun getBooksFromRemote(): BookRespone {
+    override suspend fun getBooksFromRemote(): BookResponse {
         Log.d("ApiClient", "Request URL: https://www.googleapis.com/books/v1/volumes?key=${BuildConfig.key}&q=\"''\"  ")
       val response=   ApiHelper.retrofit.create(ApiService::class.java).getBooksFromRemote(apiKey = BuildConfig.key,q="''" )
         Log.d("response", "getBooksFromRemote:$response ")
