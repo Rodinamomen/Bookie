3cd0930fd58c6b0ca1ca8c829d3f6a4673fa7f3b
Rodinamomen <rodinamobark3@gmail.com>
2024-07-30 01:25:45 -0700

    Home repo and implementation for getting data from Datasource "Remote Datasource"

--- app/src/main/java/com/example/bookie/home/repo/HomeRepoImp.kt
+++ app/src/main/java/com/example/bookie/home/repo/HomeRepoImp.kt
@@ -1,11 +1,11 @@
 package com.example.bookie.home.repo
 
 import com.example.bookie.network.RemoteDataSource
-import com.example.bookie.network.model.BooksResponse
+import com.example.bookie.network.model.BookResponse
 
 
 class HomeRepoImp(var remoteDataSource: RemoteDataSource):HomeRepo {
-    override suspend fun getBooksFromRemote(): BooksResponse {
+    override suspend fun getBooksFromRemote(): BookResponse {
         return remoteDataSource.getBooksFromRemote()
     }
 }
