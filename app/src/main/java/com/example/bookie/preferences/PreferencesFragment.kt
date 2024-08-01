f02471aa71677f03ad2ee592e0e40a05cf94eb5c
Rodinamomen <rodinamobark3@gmail.com>
2024-08-01 07:22:51 -0700

    preferences screen

---
+++ app/src/main/java/com/example/bookie/preferences/PreferencesFragment.kt
@@ -0,0 +1,21 @@
+package com.example.bookie.preferences
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bookie.R
+
+
+class PreferencesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_preferences, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/bookie/preferences/repo/PreferencesRepo.kt
@@ -0,0 +1,5 @@
+package com.example.bookie.preferences.repo
+
+interface PreferencesRepo {
+  //  suspend fun getBooksCategories(): List<String>
+}
